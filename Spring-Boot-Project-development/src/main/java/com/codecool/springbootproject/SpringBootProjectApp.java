4c5b093d6b2a9e0bec3004f38f8f4520152790de
manish1328 <jaiswalmanish2809@gmail.com>
2023-09-23 12:51:00 +0530

    Modified

--- Spring-Boot-Project-development/src/main/java/com/codecool/springbootproject/SpringBootProjectApp.java
+++ Spring-Boot-Project-development/src/main/java/com/codecool/springbootproject/SpringBootProjectApp.java
@@ -4,12 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @Slf4j
 @SpringBootApplication
 public class SpringBootProjectApp {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApp.class, args);
 		log.info("starting the app");
+		System.out.println("Start the app");
 	}
 
 }
